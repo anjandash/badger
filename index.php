85d185ca0ac77d6037ed3591c3a1556a16a14477
@anjandash <anjan8@gmail.com>
2019-11-02 16:53:44 +0100

    fc

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SuperBadger</title>
+    <link rel="shortcut icon" type="image/png" href="img/fav.png"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+        </div>
+
+        <div>
+        </div>
+
+        <div class="dash">
+            <div class="badgeSuper">
+                <div class="badgeHolder">
+                    <div class="badgeOutline">
+                        <div class="badgeImgHolder" id="veggies"></div>
+                    </div>
+                </div>
+
+                <div class="badgeText">
+                    <p>veggies</p>
+                </div>
+
+                <div class="badgeStars">
+                    <div class="star"></div>
+                    <div class="star"></div>
+                    <div class="star"></div>
+                </div>
+            </div>
+
+            <div class="badgeSuper">
+                <div class="badgeHolder">
+                    <div class="badgeOutline">
+                        <div class="badgeImgHolder" id="salad"></div>
+                    </div>
+                </div>
+
+                <div class="badgeText">
+                    <p>classic salad</p>
+                </div>
+
+                <div class="badgeStars">
+                    <div class="star"></div>
+                    <div class="star"></div>
+                    <div class="star"></div>
+                </div>
+            </div>
+
+            <div class="badgeSuper">
+                <div class="badgeHolder">
+                    <div class="badgeOutline">
+                        <div class="badgeImgHolder" id="superSalad"></div>
+                    </div>
+                </div>
+
+                <div class="badgeText">
+                    <p>super salad</p>
+                </div>
+
+                <div class="badgeStars">
+                    <div class="star"></div>
+                    <div class="star"></div>
+                    <div class="star"></div>
+                </div>
+            </div>
+
+            <div class="badgeSuper">
+                <div class="badgeHolder">
+                    <div class="badgeOutline">
+                        <div class="badgeImgHolder" id="hardWorker"></div>
+                    </div>
+                </div>
+
+                <div class="badgeText">
+                    <p>hard worker</p>
+                </div>
+
+                <div class="badgeStars">
+                    <div class="star"></div>
+                    <div class="star"></div>
+                    <div class="star"></div>
+                </div>
+            </div>
+
+
+        </div>
+    
+    </div>
+</body>
+</html>
